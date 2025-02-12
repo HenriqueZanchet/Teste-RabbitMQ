44236f91b365f08b464d1b2f95235cfc015ee501
HenriqueZanchet <henriquezmello@hotmail.com>
2025-02-12 14:13:25 -0300

    ATT: atualizacao do documento producer

--- producer.py
+++ producer.py
@@ -1,1 +1,17 @@
-producer.py
+import pika
+
+connection_parameters = pika.ConnectionParameters('localhost:15672')
+
+coneection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.chanel()
+
+channel.queue_declare(queue='letterbox')
+
+message = "esta é a minha primeira mensagem"
+
+channel.basic_publish(exchange='', routing_key='letterbox', body=message)
+
+print(f"enviando mensagem: {message}")
+
+connection.close()
