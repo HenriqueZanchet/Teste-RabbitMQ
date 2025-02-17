d6a3d4011059818449135b611e84da744613dbed
HenriqueZanchet <henriquezmello@hotmail.com>
2025-02-17 17:25:17 -0300

    comentarios sobre o código de send.py

--- ex1/send.py
+++ ex1/send.py
@@ -1,10 +1,13 @@
 import pika 
 
+# estabelece um canal de comunicação com o rabbitmq
 connection = pika.BlockingConnection(pika.ConnectionParameters('172.17.0.2'))
 channel = connection.channel()
 
+# declara uma fila nomeada de hello
 channel.queue_declare(queue='hello')
 
+# faz uma publicação básica no canal de comunicação, com routing_key sendo como uma espécie de 'identificador' pra essa rota
 channel.basic_publish(exchange='', 
                         routning_key='hello',
                         body='Hello World!')
